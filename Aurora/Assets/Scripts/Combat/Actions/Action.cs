598bbbd6e6bd1c6f4aadf8c4e7f8d5f84229da49
ProgramerBeard <jdselsor@gmail.com>
2017-08-13 21:59:53 -0700

    Started Combat System
    Started the reworking of the combat system. Started with the action system.

---
+++ Aurora/Assets/Scripts/Combat/Actions/Action.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Action {
+
+	public enum SuperTypes
+	{
+		TARGET,
+		SELF
+	};
+
+	public enum SubTypes
+	{
+		MAGICAL,
+		PHYSICAL,
+	};
+
+	[Header ("Description")]
+	public string name;
+	public int idNumber;
+	public SuperTypes superType;
+	public SubTypes[] supTypes;
+
+	[Header ("Damage")]
+	public float baseHealthDamage;
+	public float baseEnergyDamage;
+	public float baseFocusDamage;
+
+	[Header ("Cost")]
+	public float baseHealthCost;
+	public float baseEnergyCost;
+	public float baseFocusCost;
+
+	//Add the paraters
+	public void ExeuteAction () {
+		
+	}
+}
---
+++ Aurora/Assets/Scripts/Editor/CreateGameDatabases.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateGameDatabases {
+
+	[MenuItem ("Assets/Create/Action Database")]
+	public static void CreateActionDatabase () {
+		ActionDatabase db = ScriptableObject.CreateInstance <ActionDatabase> ();
+		AssetDatabase.CreateAsset (db, "Assets/Resources/ActionDatabase.asset");
+		AssetDatabase.SaveAssets ();
+	}
+}
